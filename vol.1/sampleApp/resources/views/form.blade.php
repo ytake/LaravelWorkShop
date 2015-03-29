91b052f1848917959e97f1eee27b3e59da20df0f
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2015-03-29 21:45:47 +0900

    fixed

--- vol.1/sampleApp/resources/views/form.blade.php
+++ vol.1/sampleApp/resources/views/form.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.default')
 @section('content')
-    <form method="POST" action="{{action('HomeController@postConfirm', ['aaa'])}}">
+    <form method="POST" action="{{action('HomeController@postConfirm')}}">
         <input type="hidden" name="_token" value="{{csrf_token()}}">
         {{$errors->first('name')}}
         <input type="text" name="name" id="name" value="{{Input::old('name')}}">
